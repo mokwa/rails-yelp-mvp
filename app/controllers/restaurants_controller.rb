cf97d508c4cdba59c720b1d8a09b42209e08634a
Juliana Mokwa <julimokwa@gmail.com>
2021-02-17 15:16:41 +0000

    Restaurants and reviews

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,45 @@
+class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurant.new # needed to instantiate the form_for
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
